e14840b3e19a4ce2815ff73aa4968b8e6923e0e7
Dinesh Gamage <ndmsgamage@gmail.com>
2026-02-14 10:22:22 +0530

    Initial Commit

---
+++ ServiceManager/ServiceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceManagerApp.swift
+//  ServiceManager
+//
+//  Created by Dinesh Gamage on 2026-02-14.
+//
+
+import SwiftUI
+
+@main
+struct ServiceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
